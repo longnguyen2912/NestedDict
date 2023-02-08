db31f66f3adc8ce465582b5244edc0841c001ee0
Em Nguyen <longemnguyen96@gmail.com>
2023-02-07 20:37:23 -0800

    Changed

---
+++ for_loop_basic1.py
@@ -0,0 +1,28 @@
+for i in range(-1,151):
+    print(i)
+
+for i in range(4,1001):
+    if i%5==0:
+        print(i)
+
+for i in range(0,101):
+    if i%5==0:
+        print("Coding")
+    else:
+        print(i)
+
+sum = 0
+for i in range(1, 500001, 2):
+    sum += i
+print(sum)
+
+
+for i in range (2018,0, -4):
+    print(i)
+
+lowNum = 2
+highNum = 9
+mult = 3
+for i in range(lowNum, highNum+1):
+    if i%mult==0:
+        print(i)
